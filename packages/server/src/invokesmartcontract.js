e06aa546ba223c48a4edea7655f9af1714bbd4fd
Jeroen Peeters <jeroen.peeters@evance.be>
2018-02-01 23:00:53 +0100

    chore: free transactions!

--- packages/server/src/invokesmartcontract.js
+++ packages/server/src/invokesmartcontract.js
@@ -13,12 +13,12 @@ const args = param.string('testvalue');
   const invoke = createInvoke(operation, args);
   const gasCost = 0;
   const intents = [
-    { assetId: assets.GAS, value: 0.00000001, scriptHash}
+    { assetId: assets.GAS, value: 0.00000001, scriptHash: Neon.get.scriptHashFromAddress(account.address)}
   ];
 
   // Test invoke
   const testResponse = await testInvoke(invoke);
-  if (testResponse.result.state === 'HALT, BREAK' && testResponse.result.gas_consumed < 10) {
+  if (testResponse.result.gas_consumed < 10) {
     const invokeResponse = await executeInvoke(account, invoke, gasCost, intents);
     console.log(invokeResponse);
   }
