83da231e4361c905b0db4c923f705053637bd8a6
Maurice Dalderup <Maurice_Daderup@hotmail.com>
2018-02-06 21:50:16 +0100

    chore: modified existing contract to work with the domains as described in issue #1

--- packages/smart-contract/neoblog.py
+++ packages/smart-contract/neoblog.py
@@ -1,48 +1,122 @@
 from boa.blockchain.vm.Neo.Runtime import CheckWitness
 from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put
 from boa.code.builtins import concat, substr
-
-
 ***
+TODO:
+- "Seed" smart contract -> .latest might not yet exist -> fill with 0
+- Error handling when "out of bounds" -> ex: post.1234 which does not exist
+- Add user.post, user.xxx domains on every function (Get, Put)
+- String -> int conversion and vice versa?
+- GetIndexes(domaiName (ex: "post, user, category.categoryName")) returns latest index
+- UpdateIndexes (Same as above - but updates it)
+
+Example of domain trees
+post.latest
+post.123 123 is an example post index
+post.data.1a2b 1a2b is an example IPFS file hash
+category.crypto.latest
+category.crypto.456 different post index
+user.abcd abcd is an example user address
+user.abcd.latest
+user.abcd.789 different post index
+user.userid.jeroenptrs jeroenptrs is an example user id
+
 ==========================
 ===== MAIN FUNCTIONS =====
 ==========================
-***
-***
 Storing a post hash on the BC
+  :param args: list of arguments
+    args[0] always sender hash
+    args[1] post content hash on IPFS
+  :param type: str
+***
+def submitPost(args):
+  # Default args
+  user = args[0]
+  postHash = args[1]
+
+  # Creating domains
+  postDomain = "post."
+  postLatestDomain = concat(postDomain, "latest")
+
+  # Getting and increasing lastest index
+  latestPostIndex = Get(GetContext, postLatestDomain);
+  newLatestPostIndex = latestPostIndex + 1
+  Put(GetContext, postLatestDomain, latestPostIndex)
+
+  # Insert the post on domain "post.{index}"
+  postKeyDomain = concat(postDomain, latestPostIndex)
+  Put(GetContext, postKey, postHash)
+  return True
+
+
+
+***
+Add a post to a certain category
   :param args: list of arguments
     args[0] always sender hash
     args[1] post content hash on IPFS
     args[2..11] extra categories (max 10)
   :param type: str
 ***
-def submitPost(args):
+def addPostToCategory(args):
+  # Default args
   user = args[0]
   postHash = args[1]
-  categoryList = getCategories(args)
 
-  postToStore = concat(postHash, categoryList)
+  # Getting all categories from args
+  categoryList = getCategories(args):
 
-  Put(GetContext, postToStore)
+  i = 0
+  categoryList = []
+  while i<len(categoryList):
+    categoryName = categoryList[i]
+
+    # Creating domains
+    categoryDomainAndName = concat("category.", categoryName)
+    categoryLatest = concat(categoryDomainAndName, ".latest")
+    
+    # Getting and increasing lastest index
+    oldLatestIndex = Get(GetContext, categoryLatest)
+    newLatestIndex = oldLatestIndex + 1
+    Put(GetContext, categoryValueLatest, newLatestIndex)
+
+    # Insert the post on domain "category.{categoryName}.{index}"
+    categoryNewIndex = concat(".", newLatestIndex)
+    category = concat(categoryDomainAndName, categoryNewIndex)
+    Put(GetContext, category, FILE_HASH)
   return True
 
 
 ***
-Getting the post hash from BC
+Getting the post by index from the BC
   :param args: list of arguments
     args[0] always sender hash
-    args[1] post content hash on IPFS
+    args[1] post index
   :param type: str
 ***
-def getPost(args):
+def getPostByIndex(args):
+  # Default args
   user = args[0]
-  postHash = args[1]
-  categoryList = getCategories(args)
+  postIndex = args[1]
 
-  postToGet = concat(postHash, categoryList)
+  # Getting the index and retrieving post
+  postIndexDomain = concat("post.", postIndex)
+  postByIndex = Get(GetContext, postIndexDomain)
+  return postByIndex
 
-  Get(GetContext, postToGet)
-  return True
+
+***
+Getting the latest post from the BC
+***
+def getLatestPost():
+  # Get latest index
+  latestIndex = Get(GetContext, "post.latest")
+
+  # Getting the index and retrieving post
+  lastestPostDomain = concat("post.", latestIndex)
+  latestPost = Get(GetContext, lastestPostDomain)
+  return latestPost
 
 
 
@@ -80,12 +154,12 @@ def Main(operation, args):
   ***
   Main for SC
 
-  :param operation: The function performed
-  :type operation: str
+    :param operation: The function performed
+    :type operation: str
 
-  :param args: list of arguments
-    args[0] always sender hash
-  :param type: str
+    :param args: list of arguments
+      args[0] always sender hash
+    :param type: str
 
   ***
 
@@ -97,8 +171,11 @@ def Main(operation, args):
 
   if operation != None:
     if operation == 'submitPost':
-        submitPost(args)
-        return True
+      submitPost(args)
+      return True
+    if operation == 'addPostToCategory'
+      addPostToCategory(args)
+      return True
     if operation == 'getPost':
-        getPost(args)
-        return True
+      getPost(args)
+      return True
