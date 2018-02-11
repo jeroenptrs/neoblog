a52596fe3d89b8438b045132d00367bd1d6dc2bb
Maurice Dalderup <Maurice_Daderup@hotmail.com>
2018-02-11 17:49:38 +0100

    chore: PR review update

--- packages/smart-contract/neoblog/manageUser.py
+++ packages/smart-contract/neoblog/manageUser.py
@@ -1,5 +1,4 @@
 from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put
-from boa.blockchain.vm.System.ExecutionEngine import GetCallingScriptHash
 from boa.code.builtins import concat
 
 """
@@ -11,30 +10,19 @@ def insertUser(args):
     user = args[0]
     userName = args[1]
 
-    upsert(user, userName, True)
+    upsert(user, userName)
     return True
 
 
 def updateUser(args):
     user = args[0]
+    userName = args[1]
 
-    caller = GetCallingScriptHash()
-
-    # Is the user who wants to modify a certain entry, the owner?
-    if caller == user:
-        upsert(user, userName, False)
-        return True
-    return False
+    upsert(user, userName)
+    return True
 
 
-def upsert(user, userName, checkNeeded):
+def upsert(user, userName):
     upsertDomain = concat("user.userId.", userName)
-
-    # Validate the username does not exist
-    if checkNeeded:
-        condition = Get(GetContext, upsertDomain)
-        if condition != ''
-            return False
-
     Put(GetContext, upsertDomain, user)
     return True
