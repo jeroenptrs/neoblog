0a724f1fbb99b58a4b5c5de0646566aeec658913
Jeroen Peeters <jeroen.peeters@evance.be>
2018-02-12 12:02:38 +0100

    chore: more pretty code

--- packages/client/src/components/views/views.jsx
+++ packages/client/src/components/views/views.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 // models
-import { Route } from 'mobx-router';
+import { Route } from "mobx-router";
 // views
-import Home from './Home';
-import NewPost from './NewPost';
-import ArticleView from './ArticleView';
+import Home from "./Home";
+import NewPost from "./NewPost";
+import ArticleView from "./ArticleView";
 
 const views = {
   home: new Route({
-    path: '/',
-    component: <Home />,
+    path: "/",
+    component: <Home />
   }),
   newPost: new Route({
-    path: '/newPost',
-    component: <NewPost />,
+    path: "/newPost",
+    component: <NewPost />
   }),
   articleView: new Route({
-    path: '/article/:fileHash',
-    component: <ArticleView />,
-  }),
+    path: "/article/:fileHash",
+    component: <ArticleView />
+  })
 };
 
 export default views;
