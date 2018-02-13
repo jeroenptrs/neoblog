245e873df796ef90c4e58dfee9c0544f8a245235
Jeroen Peeters <jeroen.peeters@evance.be>
2018-02-13 09:19:26 +0100

    chore: move fluid type to article view

--- packages/client/src/components/Article/Article.jsx
+++ packages/client/src/components/Article/Article.jsx
@@ -6,6 +6,8 @@ import ReactMarkdown from "react-markdown";
 // Styles
 import "./Article.css";
 
-const Article = props => <ReactMarkdown source={props.source} />;
+const Article = props => (
+  <ReactMarkdown className="text-content" source={props.source} />
+);
 
 export default inject("store")(observer(Article));
