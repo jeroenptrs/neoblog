759cf5d8602552a055aa49af1ea4fc2ce352eac9
Jeroen Peeters <jeroen.peeters@evance.be>
2018-02-17 18:54:40 +0100

    chore: init menu

---
+++ packages/client/src/components/MenuShell/MenuShell.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { inject, observer } from "mobx-react";
+
+// Components
+import Signin from "../Signin/Signin";
+
+// Styles
+import "./MenuShell.css";
+
+const MenuShell = props => {
+  const { signedIn } = props.store.app.states;
+  if (signedIn) return <div className="neoblogMenu">U mama boi</div>;
+  return <Signin />;
+};
+
+export default inject("store")(observer(MenuShell));
